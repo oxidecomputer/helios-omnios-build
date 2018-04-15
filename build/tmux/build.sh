bd5e671eab69677c71941b60f36bfd14614e88b7
Andy Fiddaman <omnios@citrus-it.co.uk>
2018-04-15 20:09:56 +0000

    tmux: update to version 2.7

--- build/tmux/build.sh
+++ build/tmux/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 #
-# CDDL HEADER START
+# {{{ CDDL HEADER START
 #
 # The contents of this file are subject to the terms of the
 # Common Development and Distribution License, Version 1.0 only
@@ -18,67 +18,68 @@
 # fields enclosed by brackets "[]" replaced with your own identifying
 # information: Portions Copyright [yyyy] [name of copyright owner]
 #
-# CDDL HEADER END
-#
+# CDDL HEADER END }}}
 #
 # Copyright 2016 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Copyright 2018 OmniOS Community Edition (OmniOSce) Association.
 # Use is subject to license terms.
 #
-# Load support functions
 . ../../lib/functions.sh
 
 PROG=tmux
-VER=2.6
+VER=2.7
 VERHUMAN=$VER
 PKG=terminal/tmux
-SUMMARY="terminal multiplexer"
+SUMMARY="Terminal multiplexer"
 DESC="$SUMMARY"
 LIBEVENT_VER=2.1.8
 LDIR=libevent-${LIBEVENT_VER}-stable
 XFORM_ARGS+=" -DLIBEVENT=$LIBEVENT_VER"
 
 BUILDARCH=32
-CONFIGURE_OPTS_32="$CONFIGURE_OPTS_32 --bindir=/usr/bin"
-CPPFLAGS="-I$TMPDIR/$PROG-$VER/$LDIR/include/event2 \
-    -I$TMPDIR/$PROG-$VER/$LDIR/include -I/usr/include/ncurses"
-CFLAGS="-std=c99 -D_XPG6 -D_POSIX_C_SOURCE=200112L"
+CONFIGURE_OPTS_32+=" --bindir=/usr/bin"
+CPPFLAGS="\
+    -I$TMPDIR/$PROG-$VER/$LDIR/include/event2 \
+    -I$TMPDIR/$PROG-$VER/$LDIR/include -I/usr/include/ncurses \
+"
 LDFLAGS="-L$TMPDIR/$PROG-$VER/$LDIR/.libs -lsocket -lnsl -lsendfile"
 
-save_function configure32 configure32_orig
-configure32(){
-  pushd $TMPDIR/$BUILDDIR/$LDIR > /dev/null
-  logmsg "configuring libevent"
-  logcmd ./configure --disable-static --disable-libevent-install || \
-    logerr "failed libevent configure"
-  logmsg "building a static libevent"
+build_libevent() {
+    pushd $TMPDIR/$BUILDDIR/$LDIR > /dev/null
+    logmsg "--- configuring libevent"
+    logcmd ./configure --disable-static --disable-libevent-install \
+        || logerr "failed libevent configure"
 
-  # We have to patch libevent as well. Change PATCHDIR for now...
-  oBUILDDIR=$BUILDDIR
-  BUILDDIR+="/$LDIR"
-  PATCHDIR=patches-libevent
-  patch_source
-  PATCHDIR=patches
-  BUILDDIR=$oBUILDDIR
+    # We have to patch libevent as well. Change PATCHDIR temporarily.
+    oBUILDDIR=$BUILDDIR
+    BUILDDIR+="/$LDIR"
+    PATCHDIR=patches-libevent
+    patch_source
+    PATCHDIR=patches
+    BUILDDIR=$oBUILDDIR
 
-  logcmd make || logerr "failed libevent build"
-  popd > /dev/null
-  configure32_orig
+    logmsg "--- building libevent"
+    logcmd make || logerr "failed libevent build"
+    popd > /dev/null
 }
 
 init
 download_source $PROG $PROG $VER
-MAINBUILDDIR=$BUILDDIR
+
+# Download and build libevent as that is not part of core
+_BUILDDIR=$BUILDDIR
 BUILDDIR=$LDIR
-download_source libevent libevent ${LIBEVENT_VER}-stable $TMPDIR/$PROG-$VER
-BUILDDIR=$MAINBUILDDIR
+download_source libevent libevent ${LIBEVENT_VER}-stable $TMPDIR/$_BUILDDIR
+BUILDDIR=$_BUILDDIR
+build_libevent
+
 patch_source
 prep_build
 build
 make_isa_stub
 strip_install
-VER=${VER//a/.0}
 make_package
 clean_up
 
 # Vim hints
-# vim:ts=4:sw=4:et:
+# vim:ts=4:sw=4:et:fdm=marker
