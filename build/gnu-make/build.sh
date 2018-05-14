e1e5d76802a7b8e57b72aa063c99bcbe2520221c
Andy Fiddaman <omnios@citrus-it.co.uk>
2018-05-14 09:18:19 +0000

    GNU make - 64-bit, default to CC=gcc, clean testsuite output

--- build/gnu-make/build.sh
+++ build/gnu-make/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 #
-# CDDL HEADER START
+# {{{ CDDL HEADER START
 #
 # The contents of this file are subject to the terms of the
 # Common Development and Distribution License, Version 1.0 only
@@ -18,14 +18,12 @@
 # fields enclosed by brackets "[]" replaced with your own identifying
 # information: Portions Copyright [yyyy] [name of copyright owner]
 #
-# CDDL HEADER END
-#
+# CDDL HEADER END }}}
 #
 # Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
-# Copyright 2017 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2018 OmniOS Community Edition (OmniOSce) Association.
 # Use is subject to license terms.
 #
-# Load support functions
 . ../../lib/functions.sh
 
 PROG=make
@@ -33,16 +31,28 @@ VER=4.2.1
 VERHUMAN=$VER
 PKG=developer/build/gnu-make
 SUMMARY="gmake - GNU make"
-DESC="GNU make - A utility used to build software (gmake)"
+DESC="GNU make - A utility used to build software"
 
 RUN_DEPENDS_IPS="system/prerequisite/gnu"
 
-BUILDARCH=32
+BUILDARCH=64
 CONFIGURE_OPTS="--bindir=$PREFIX/bin --program-prefix=g"
+
+TESTSUITE_SED="
+    /-srcdir/d
+    /Making /d
+    /on SunOS/d
+    /getloadavg/d
+    /getlogin_r/d
+    /load average/d
+    /1-minute/d
+    /~~~~~~~~~/d
+"
 
 init
 download_source $PROG $PROG $VER
 patch_source
+run_autoreconf -fi      # As Makefile.am has been modified
 prep_build
 build
 run_testsuite check
@@ -51,4 +61,4 @@ make_package
 clean_up
 
 # Vim hints
-# vim:ts=4:sw=4:et:
+# vim:ts=4:sw=4:et:fdm=marker
