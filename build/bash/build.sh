d6420784a21e8b40416615e13be072eb0602b7ab
Andy Fiddaman <omnios@citrus-it.co.uk>
2018-06-01 22:39:56 +0000

    bash: update to 4.4.23

--- build/bash/build.sh
+++ build/bash/build.sh
@@ -29,7 +29,7 @@
 
 PROG=bash
 VER=4.4
-PATCHLEVEL=19
+PATCHLEVEL=23
 VERHUMAN="$VER.$PATCHLEVEL"
 PKG=shell/bash
 SUMMARY="GNU Bourne-Again shell (bash)"
@@ -53,42 +53,42 @@ CONFIGURE_OPTS="
     --enable-alias
     --enable-arith-for-command
     --enable-array-variables
-    --enable-bang-history   
-    --enable-brace-expansion    
-    --enable-casemod-attributes 
-    --enable-casemod-expansions 
-    --enable-command-timing 
-    --enable-cond-command   
-    --enable-cond-regexp    
-    --enable-coprocesses    
-    --enable-debugger   
-    --enable-directory-stack    
-    --enable-disabled-builtins  
-    --enable-dparen-arithmetic  
-    --enable-extended-glob  
-    --enable-help-builtin   
-    --enable-history    
-    --enable-job-control    
-    --enable-multibyte  
-    --enable-net-redirections   
-    --enable-process-substitution   
-    --enable-progcomp   
-    --enable-prompt-string-decoding 
-    --enable-readline   
-    --enable-restricted 
-    --enable-select 
-    --enable-separate-helpfiles 
-    --enable-single-help-strings    
-    --disable-strict-posix-default  
-    --enable-usg-echo-default   
-    --enable-xpg-echo-default   
-    --enable-mem-scramble   
-    --disable-profiling 
+    --enable-bang-history
+    --enable-brace-expansion
+    --enable-casemod-attributes
+    --enable-casemod-expansions
+    --enable-command-timing
+    --enable-cond-command
+    --enable-cond-regexp
+    --enable-coprocesses
+    --enable-debugger
+    --enable-directory-stack
+    --enable-disabled-builtins
+    --enable-dparen-arithmetic
+    --enable-extended-glob
+    --enable-help-builtin
+    --enable-history
+    --enable-job-control
+    --enable-multibyte
+    --enable-net-redirections
+    --enable-process-substitution
+    --enable-progcomp
+    --enable-prompt-string-decoding
+    --enable-readline
+    --enable-restricted
+    --enable-select
+    --enable-separate-helpfiles
+    --enable-single-help-strings
+    --disable-strict-posix-default
+    --enable-usg-echo-default
+    --enable-xpg-echo-default
+    --enable-mem-scramble
+    --disable-profiling
     --enable-largefile
-    --enable-nls    
-    --with-bash-malloc  
-    --with-curses   
-    --with-installed-readline=no    
+    --enable-nls
+    --with-bash-malloc
+    --with-curses
+    --with-installed-readline=no
 "
 reset_configure_opts
 
