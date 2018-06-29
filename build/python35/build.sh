58a6dba79f1b37f62336c14118f6ef5829c53603
Andy Fiddaman <omnios@citrus-it.co.uk>
2018-06-29 15:50:05 +0000

    Python: move to 64-bit only

--- build/python35/build.sh
+++ build/python35/build.sh
@@ -38,12 +38,19 @@ RUN_DEPENDS_IPS="
 "
 XFORM_ARGS="-D PYTHONVER=$MVER"
 
+set_arch 64
+
 export CCSHARED="-fPIC"
-LDFLAGS32="-L/usr/gnu/lib -R/usr/gnu/lib"
 LDFLAGS64="-L/usr/gnu/lib/amd64 -R/usr/gnu/lib/amd64"
 CPPFLAGS+=" -I/usr/include/ncurses -D_LARGEFILE64_SOURCE"
 CPPFLAGS64="`pkg-config --cflags libffi`"
-CPPFLAGS32="${CPPFLAGS64/amd64?/}"
+CC+=' -m64'
+export DFLAGS=-64
+MAKE_ARGS="
+    DFLAGS=-64
+    DESTSHARED=/usr/lib/python$MVER/lib-dynload
+"
+MAKE_INSTALL_ARGS=DESTSHARED=/usr/lib/python$MVER/lib-dynload
 
 CONFIGURE_OPTS="
     --enable-shared
@@ -51,6 +58,7 @@ CONFIGURE_OPTS="
     --with-system-ffi
     --with-system-expat
     --enable-ipv6
+    --without-ensurepip
 "
 
 # We patch auto* files so need to re-generate
@@ -62,107 +70,15 @@ preprep_build() {
         || logerr "Could not set pydtrace_offsets.sh executable"
 }
 
-# Need to set CC='$CC -m64' for 64-bit build in order that macros such as
-# LONG_BIT are correctly set
-save_function configure64 _configure64
-configure64() {
-    CC="$CC -m64" _configure64
-}
-
-make_prog32() {
-    MAKE_ARGS="DFLAGS=-32"
-    export DFLAGS=-32
-    make_prog
-}
-
-make_prog64() {
-    # With Python 3, LIBPL is hard-coded rather than being based on LIBDIR
-    # As a result, the 64-bit files clobber the 32-bit ones and break the
-    # extension builder. Move the 64-bit files to /usr/lib/amd64/...
-    LIBPL="`grep LIBPL $DESTDIR/usr/lib/python$MVER/_sysconfigdata-32.py \
-        | cut -d"'" -f4 | sed 's^lib/^&amd64/^'`"
-    MAKE_ARGS="
-        DFLAGS=-64
-        DESTSHARED=/usr/lib/python$MVER/lib-dynload
-        LIBPL=$LIBPL
-    "
-    export DFLAGS=-64
-    make_prog
-}
-
-# Parts of python are architecture dependant. This function saves the
-# installed versions after each arch build for later manipulation.
-save_arch() {
-    bits=$1
-    pushd $DESTDIR > /dev/null || logerr "cd $DESTDIR"
-
-    # Save arch-specific sysconfigdata
-    if [ "$bits" = 32 ]; then
-        mv usr/lib/python$MVER/_sysconfigdata{.py,-$bits.py} \
-            || logerr "Cannot archive $bits-bit sysconfigdata"
-    else
-        mv  usr/lib/python$MVER/lib-dynload/64/_sysconfigdata.py \
-            usr/lib/python$MVER/_sysconfigdata-$bits.py \
-            || logerr "Cannot archive $bits-bit sysconfigdata"
-    fi
-    # Save arch-specific pyconfig.h
-    mv usr/include/python${MVER}m/pyconfig{.h,-$bits.h} \
-        || logerr "Cannot archive $bits-bit pyconfig.h"
-    # Save config tree
-    LIBPL="`grep LIBPL usr/lib/python$MVER/_sysconfigdata-$bits.py \
-        | cut -d"'" -f4 | cut -c2-`"
-    rsync -a $LIBPL/ $LIBPL-$bits/
-
-    popd > /dev/null
-}
-
-generate_archdeps() {
-    logmsg "Fixing architecture-dependent bits"
-
-    # Copy in arch-detecting pyconfig.h
-    sed < $SRCDIR/files/pyconfig.h \
-        >$DESTDIR/usr/include/python${MVER}m/pyconfig.h "
-            s/_MVER_/$MVER/g
-        " || logerr "Cannot install pyconfig.h"
-
-    # Restore config trees
-    pushd $DESTDIR >/dev/null || logerr "Cannot chdir"
-    LIBPL="`grep LIBPL usr/lib/python$MVER/_sysconfigdata-32.py \
-        | cut -d"'" -f4 | cut -c2-`"
-    logmsg "-- config tree at $LIBPL"
-    logcmd rm -rf $LIBPL
-    logcmd mv $LIBPL-32 $LIBPL || logerr "libpl 32"
-    LIBPL64=`echo $LIBPL | sed 's^lib/^&amd64/^'`
-    logcmd mkdir -p `dirname $LIBPL64`
-    logcmd mv $LIBPL-64 $LIBPL64 || logerr "libpl 64"
-    sed -i '/LIBPL/s^lib/^&amd64/^' usr/lib/python$MVER/_sysconfigdata-64.py \
-        || logerr "libpl 64 sed"
-    popd >/dev/null
-
-    # Generate 32/64-bit agile _sysconfigdata.py
-    pushd $DESTDIR/usr/lib/python$MVER >/dev/null || logerr "Cannot chdir"
-    cat << EOM > _sysconfigdata.py
-import sys
-
-if sys.maxsize > 2**32:
-`sed 's/^/    /' < _sysconfigdata-64.py`
-else:
-`sed 's/^/    /' < _sysconfigdata-32.py`
-EOM
-    rm -f __pycache__/_sysconfigdata*
-    popd > /dev/null
-}
-
-#TESTSUITE_SED="
-#    s/.*load avg: .*\] /  /
-#    /DeprecationWarning.*exc_clear/d
-#    s/ passed in [0-9].*//
-#    /No differences encountered/d
-#"
+TESTSUITE_SED="
+    1,/tests OK/ {
+        /tests OK/p
+        d
+    }
+"
 
 launch_testsuite() {
-    # Skip network tests. Many of them rely on an open Internet connection.
-    export EXTRATESTOPTS="-uall,-network"
+    export EXTRATESTOPTS="-uall,-network,-audio,-gui,-largefile"
     # Some tests have non-ASCII characters
     _LC_ALL=$LC_ALL
     export LC_ALL=en_US.UTF-8
@@ -170,16 +86,10 @@ launch_testsuite() {
     export LC_ALL=$_LC_ALL
 }
 
-make_install32() {
-    make_install
-    save_arch 32
-    launch_testsuite test "" testsuite-32.log
-}
-
-make_install64() {
-    MAKE_INSTALL_ARGS=DESTSHARED=/usr/lib/python$MVER/lib-dynload make_install
-    save_arch 64
-    launch_testsuite
+test_dtrace() {
+    pushd $TMPDIR/$BUILDDIR > /dev/null
+    $PFEXEC $SRCDIR/files/run-dtrace-tests | tee $SRCDIR/testsuite-d.log
+    popd
 }
 
 init
@@ -188,9 +98,9 @@ patch_source
 preprep_build
 prep_build
 build
-generate_archdeps
-make_isa_stub
 strip_install
+launch_testsuite
+test_dtrace
 make_package
 clean_up
 
