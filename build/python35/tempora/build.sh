925260b77b0d5202b08c80e37822497b19c972a2
Andy Fiddaman <omnios@citrus-it.co.uk>
2018-11-12 10:32:27 +0000

    py/tempora: update to 1.14

--- build/python35/tempora/build.sh
+++ build/python35/tempora/build.sh
@@ -18,8 +18,8 @@
 
 PKG=library/python-3/tempora-35
 PROG=tempora
-VER=1.13
-SUMMARY="tempora - Objects and routines pertaining to date and time"
+VER=1.14
+SUMMARY="Objects and routines pertaining to date and time"
 DESC="$SUMMARY"
 
 . $SRCDIR/../common.sh
