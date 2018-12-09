b317a3149b3f4f63bf643b5a7bfb41c02f626e39
Dominik Hassler <hadfl@omniosce.org>
2018-12-09 17:51:19 +0100

    nghttp2: update to 1.35.1

--- build/nghttp2/build.sh
+++ build/nghttp2/build.sh
@@ -27,7 +27,7 @@
 . ../../lib/functions.sh
 
 PROG=nghttp2
-VER=1.35.0
+VER=1.35.1
 PKG=library/nghttp2
 SUMMARY="nghttp2: HTTP/2 C Library"
 DESC="An implementation of the Hypertext Transfer Protocol version 2 in C"
