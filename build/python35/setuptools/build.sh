b6af237cee917897c8398a18680918b0c52ed325
Dominik Hassler <hadfl@omniosce.org>
2019-02-21 21:53:30 +0100

    setuptools: update to 40.8.0

--- build/python35/setuptools/build.sh
+++ build/python35/setuptools/build.sh
@@ -18,7 +18,7 @@
 
 PKG=library/python-3/setuptools-35
 PROG=setuptools
-VER=40.7.2
+VER=40.8.0
 SUMMARY="Python package management"
 DESC="Easily download, build, install, upgrade, and uninstall Python packages"
 
