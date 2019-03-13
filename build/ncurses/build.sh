7d4efc27f6f868339028f6d4058bd30efbfb5817
Andy Fiddaman <omnios@citrus-it.co.uk>
2019-03-13 12:56:56 +0000

    ncurses: update to 20190309

--- build/ncurses/build.sh
+++ build/ncurses/build.sh
@@ -27,7 +27,7 @@
 . ../../lib/functions.sh
 
 PROG=ncurses
-VER=6.1-20190216
+VER=6.1-20190309
 PKG=library/ncurses
 SUMMARY="A CRT screen handling package"
 DESC="Utilities and shared libraries for terminal handling"
