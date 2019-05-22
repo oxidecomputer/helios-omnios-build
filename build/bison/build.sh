3e628de4899d0df655a5e2340611cb75262bd490
Dominik Hassler <hadfl@omniosce.org>
2019-05-22 19:56:36 +0200

    bison: update to 3.4.1

--- build/bison/build.sh
+++ build/bison/build.sh
@@ -26,7 +26,7 @@
 . ../../lib/functions.sh
 
 PROG=bison
-VER=3.4
+VER=3.4.1
 PKG=developer/parser/bison
 SUMMARY="General-purpose parser generator"
 DESC="A general-purpose parser generator that converts an annotated "
