2fcf29a6c6ffc4f6f8be6b7a33c573a90690d435
Andy Fiddaman <omnios@citrus-it.co.uk>
2019-06-20 09:55:19 +0000

    expat: update to 2.2.7

--- build/expat/build.sh
+++ build/expat/build.sh
@@ -21,38 +21,40 @@
 # CDDL HEADER END }}}
 #
 # Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
-# Copyright 2018 OmniOS Community Edition (OmniOSce) Association.
-# Use is subject to license terms.
+# Copyright 2019 OmniOS Community Edition (OmniOSce) Association.
 #
 . ../../lib/functions.sh
 
 PROG=expat
-VER=2.2.6
+VER=2.2.7
 PKG=library/expat
 SUMMARY="XML parser library"
 DESC="Fast streaming XML parser written in C"
 
-CONFIGURE_OPTS_64+=" --includedir=/usr/include"
+CONFIGURE_OPTS_64+="
+    --bindir=/usr/bin
+"
+
+TESTSUITE_SED="
+    /^[^#]/d
+"
 
 save_function make_clean _make_clean
 make_clean() {
     # As of expat 2.2.4, distclean removes the generated xmlwf.1
     # man page too so that it is re-generated during build using
     # docbook2X. We don't have docbook2X so preserve the file.
-    [ -f doc/xmlwf.1~ ] || cp doc/xmlwf.1 doc/xmlwf.1~
+    [ -f doc/xmlwf.1~ ] || cp doc/xmlwf.1{,~}
     _make_clean
-    [ -f doc/xmlwf.1 ] || cp doc/xmlwf.1~ doc/xmlwf.1
+    [ -f doc/xmlwf.1 ] || cp doc/xmlwf.1{~,}
 }
 
 init
 download_source $PROG $PROG $VER
 patch_source
-run_autoreconf
 prep_build
 build
 run_testsuite check
-make_isa_stub
-sync
 make_package
 clean_up
 
