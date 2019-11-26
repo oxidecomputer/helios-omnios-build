f733bfbd21a3fc46893bf1289333f18dbedce201
Dominik Hassler <hadfl@omniosce.org>
2019-11-26 21:04:35 +0000

    nss: update to 3.47.1

--- build/mozilla-nss-nspr/build.sh
+++ build/mozilla-nss-nspr/build.sh
@@ -18,7 +18,7 @@
 . ../../lib/functions.sh
 
 PROG=nss
-VER=3.47
+VER=3.47.1
 # Include NSPR version since we're downloading a combined tarball.
 NSPRVER=4.23
 # But set BUILDDIR to just be the NSS version.
