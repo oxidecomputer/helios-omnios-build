6cf6997b00ca5e9fc911e987eb2b828251cb6892
Dominik Hassler <hadfl@omniosce.org>
2020-01-11 00:29:27 +0000

    importlib-metadata-37: update to 1.4.0

--- build/python37/importlib-metadata/build.sh
+++ build/python37/importlib-metadata/build.sh
@@ -12,13 +12,13 @@
 # http://www.illumos.org/license/CDDL.
 # }}}
 #
-# Copyright 2019 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2020 OmniOS Community Edition (OmniOSce) Association.
 
 . ../../../lib/functions.sh
 
 PKG=library/python-3/importlib-metadata-37
 PROG=importlib-metadata
-VER=1.3.0
+VER=1.4.0
 SUMMARY="Read metadata from Python packages"
 DESC="A library to access the metadata for a Python package"
 
