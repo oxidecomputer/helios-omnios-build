15f416ad9b399c6beebf257ec9b89934867fd94f
Dominik Hassler <hadfl@omniosce.org>
2020-01-16 18:47:47 +0100

    ncurses: update to 6.1.20200111

--- build/ncurses/build.sh
+++ build/ncurses/build.sh
@@ -18,7 +18,7 @@
 . ../../lib/functions.sh
 
 PROG=ncurses
-VER=6.1-20191228
+VER=6.1-20200111
 PKG=library/ncurses
 SUMMARY="A CRT screen handling package"
 DESC="Utilities and shared libraries for terminal handling"
