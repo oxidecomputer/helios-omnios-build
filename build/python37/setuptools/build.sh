72fcabd19e06554d673863eff9e556719b73696b
Dominik Hassler <hadfl@omniosce.org>
2020-01-25 13:44:13 +0000

    setuptools: update to 45.1.0

--- build/python37/setuptools/build.sh
+++ build/python37/setuptools/build.sh
@@ -18,7 +18,7 @@
 
 PKG=library/python-3/setuptools-37
 PROG=setuptools
-VER=45.0.0
+VER=45.1.0
 SUMMARY="Python package management"
 DESC="Easily download, build, install, upgrade, and uninstall Python packages"
 
