72656cd26a7d99d102e318aa67dd783ae563d546
Dominik Hassler <hadfl@omniosce.org>
2020-02-01 16:10:42 +0000

    sudo: update to 1.8.31

--- build/sudo/build.sh
+++ build/sudo/build.sh
@@ -18,7 +18,7 @@
 . ../../lib/functions.sh
 
 PROG=sudo
-VER=1.8.30
+VER=1.8.31
 PKG=security/sudo
 SUMMARY="Authority delegation tool"
 DESC="Provide limited super-user privileges to specific users"
