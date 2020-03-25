dcd804f7b2b828011d8db6be473da656fb2bb1d0
Dominik Hassler <hadfl@omniosce.org>
2020-03-25 17:29:29 +0000

    git: update to 2.26.0

--- build/git/build.sh
+++ build/git/build.sh
@@ -19,7 +19,7 @@
 . ../../lib/functions.sh
 
 PROG=git
-VER=2.25.2
+VER=2.26.0
 PKG=developer/versioning/git
 SUMMARY="$PROG - distributed version control system"
 DESC="Git is a free and open source distributed version control system "
