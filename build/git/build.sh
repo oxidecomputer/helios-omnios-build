f64b80fd61281210d1aa6af5e2e3e5578ff6ed4d
Andy Fiddaman <omnios@citrus-it.co.uk>
2020-04-22 09:13:45 +0000

    git: update to 2.26.2

--- build/git/build.sh
+++ build/git/build.sh
@@ -19,7 +19,7 @@
 . ../../lib/functions.sh
 
 PROG=git
-VER=2.26.1
+VER=2.26.2
 PKG=developer/versioning/git
 SUMMARY="$PROG - distributed version control system"
 DESC="Git is a free and open source distributed version control system "
