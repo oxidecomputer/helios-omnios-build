9f61e306427359937722c5bfa3bd171941c1092c
Andy Fiddaman <omnios@citrus-it.co.uk>
2020-05-04 19:57:07 +0000

    Clean up developer/macro/cpp package

--- build/cpp/build.sh
+++ build/cpp/build.sh
@@ -1,28 +1,19 @@
 #!/usr/bin/bash
 #
-# {{{ CDDL HEADER START
+# {{{ CDDL HEADER
 #
-# The contents of this file are subject to the terms of the
-# Common Development and Distribution License, Version 1.0 only
-# (the "License").  You may not use this file except in compliance
-# with the License.
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL"), version 1.0.
+# You may only use this file in accordance with the terms of version
+# 1.0 of the CDDL.
 #
-# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
-# or http://www.opensolaris.org/os/licensing.
-# See the License for the specific language governing permissions
-# and limitations under the License.
-#
-# When distributing Covered Code, include this CDDL HEADER in each
-# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
-# If applicable, add the following below this CDDL HEADER, with the
-# fields enclosed by brackets "[]" replaced with your own identifying
-# information: Portions Copyright [yyyy] [name of copyright owner]
-#
-# CDDL HEADER END }}}
+# A full copy of the text of the CDDL should have accompanied this
+# source. A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+# }}}
 #
 # Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
-# Use is subject to license terms.
-# Copyright 2018 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2020 OmniOS Community Edition (OmniOSce) Association.
 #
 . ../../lib/functions.sh
 
@@ -35,33 +26,18 @@ DESC="$SUMMARY"
 BUILD_DEPENDS_IPS="developer/parser/bison"
 SKIP_LICENCES="*.licence"
 
+# cpp does not work properly when built 64-bit
 set_arch 32
+set_builddir $PROG
 
-setup_src() {
-   logcmd mkdir -p $TMPDIR/$BUILDDIR
-   logcmd cp $SRCDIR/files/* $TMPDIR/$BUILDDIR || logerr "file copy failed"
-}
-
-build() {
-    # Set the version to something reasonable
-    pushd $TMPDIR/$BUILDDIR > /dev/null || logerr "can't enter build harness"
-    logcmd gmake CC=gcc CFLAGS=\"$CFLAGS32\"
-    popd > /dev/null
-}
-
-make_install() {
-    logcmd mkdir -p $DESTDIR/usr/lib || logerr "mkdir failed"
-    logcmd mkdir -p $DESTDIR/usr/ccs/lib || logerr "mkdir failed"
-    pushd $TMPDIR/$BUILDDIR > /dev/null || logerr "can't enter build harness"
-    logcmd gmake install CC=gcc DESTDIR=$DESTDIR
-    popd > /dev/null
+configure32() {
+    export CFLAGS="$CFLAGS32"
 }
 
 init
-setup_src
+download_source cpp cpp
 prep_build
 build
-make_install
 make_package
 clean_up
 
