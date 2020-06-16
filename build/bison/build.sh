fcae3127c64d0853b9790b92a17aa9016169f80c
Andy Fiddaman <omnios@citrus-it.co.uk>
2020-06-16 18:04:26 +0000

    bison: update to 3.6.4

--- build/bison/build.sh
+++ build/bison/build.sh
@@ -18,7 +18,7 @@
 . ../../lib/functions.sh
 
 PROG=bison
-VER=3.6.3
+VER=3.6.4
 PKG=developer/parser/bison
 SUMMARY="General-purpose parser generator"
 DESC="A general-purpose parser generator that converts an annotated "
