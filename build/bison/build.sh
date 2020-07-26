6c876634a9fa45c1ac310f2f7505a399bce4ed34
Andy Fiddaman <omnios@citrus-it.co.uk>
2020-07-26 00:17:52 +0000

    bison: update to 3.7

--- build/bison/build.sh
+++ build/bison/build.sh
@@ -18,7 +18,7 @@
 . ../../lib/functions.sh
 
 PROG=bison
-VER=3.6.4
+VER=3.7
 PKG=developer/parser/bison
 SUMMARY="General-purpose parser generator"
 DESC="A general-purpose parser generator that converts an annotated "
@@ -44,7 +44,7 @@ patch_source
 prep_build
 build
 strip_install
-PATH+=:/opt/ooce/bin run_testsuite check
+PATH=/usr/gnu/bin:/opt/ooce/bin:$PATH run_testsuite check
 make_package
 clean_up
 
