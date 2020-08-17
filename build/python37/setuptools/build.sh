91c792c083311be2109e4b96b58e661014cba503
Andy Fiddaman <omnios@citrus-it.co.uk>
2020-08-17 10:40:40 +0000

    setuptools: update to 49.6.0

--- build/python37/setuptools/build.sh
+++ build/python37/setuptools/build.sh
@@ -18,7 +18,7 @@
 
 PKG=library/python-3/setuptools-37
 PROG=setuptools
-VER=49.2.0
+VER=49.6.0
 SUMMARY="Python package management"
 DESC="Easily download, build, install, upgrade, and uninstall Python packages"
 
