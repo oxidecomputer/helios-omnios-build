9e51accffe3b58d941e5177318effd0c4d3ccd08
Dominik Hassler <hadfl@omnios.org>
2020-11-03 17:21:46 +0000

    nss: update to 3.58

--- build/mozilla-nss-nspr/build.sh
+++ build/mozilla-nss-nspr/build.sh
@@ -18,7 +18,7 @@
 . ../../lib/functions.sh
 
 PROG=nss
-VER=3.57
+VER=3.58
 # Include NSPR version since we're downloading a combined tarball.
 NSPRVER=4.29
 # But set BUILDDIR to just be the NSS version.
