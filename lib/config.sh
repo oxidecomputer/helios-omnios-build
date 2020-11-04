beb85b50850b2878bbfd8760541be0c97d3ff5cd
Andy Fiddaman <omnios@citrus-it.co.uk>
2020-11-04 10:28:03 +0000

    Start new r151037 cycle

--- lib/config.sh
+++ lib/config.sh
@@ -60,7 +60,7 @@ LC_TIME=C;      export LC_TIME
 ######################################################################
 
 # Default branch
-RELVER=151035
+RELVER=151037
 DASHREV=0
 PVER=$RELVER.$DASHREV
 
