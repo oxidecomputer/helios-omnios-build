8101c356153d36819f6a2127cb95f01f21bc4c38
Andy Fiddaman <omnios@citrus-it.co.uk>
2020-11-12 12:06:00 +0000

    setuptools: update to 50.3.2

--- build/python39/setuptools/build.sh
+++ build/python39/setuptools/build.sh
@@ -18,7 +18,7 @@
 
 PKG=library/python-3/setuptools-39
 PROG=setuptools
-VER=50.3.0
+VER=50.3.2
 SUMMARY="Python package management"
 DESC="Easily download, build, install, upgrade, and uninstall Python packages"
 
