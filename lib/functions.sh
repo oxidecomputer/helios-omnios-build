d4fe0f1845e46700a530d9fc33fa109f75b2b20d
Andy Fiddaman <omnios@citrus-it.co.uk>
2020-11-28 10:21:05 +0000

    Support skipping CTF for some objects

--- lib/functions.sh
+++ lib/functions.sh
@@ -2503,6 +2503,11 @@ convert_ctf() {
     while read file; do
         file $file | $EGREP -s ':	ELF' || continue
 
+        if [ -n "$CTFSKIP" ] && echo $file | $EGREP -s "$CTFSKIP"; then
+            logmsg "$ctftag skipped $file"
+            continue
+        fi
+
         if $CTFDUMP -h "$file" 1>/dev/null 2>&1; then
             continue
         fi
@@ -2725,7 +2730,7 @@ check_ssp() {
     : > $TMPDIR/rtime.ssp
     while read obj; do
         [ -f "$destdir/$obj" ] || continue
-        nm $destdir/$obj | egrep -s '__stack_chk_guard' \
+        nm $destdir/$obj | $EGREP -s '__stack_chk_guard' \
             || echo "$obj does not include stack smashing protection" \
             >> $TMPDIR/rtime.ssp &
         parallelise $LCPUS
