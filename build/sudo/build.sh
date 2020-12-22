86a8b51ca6caf6fc9c96ca5fb7e4d033f6a83d0e
Dominik Hassler <hadfl@omnios.org>
2020-12-22 05:17:04 +0000

    sudo: update to 1.9.4p2

--- build/sudo/build.sh
+++ build/sudo/build.sh
@@ -18,7 +18,7 @@
 . ../../lib/functions.sh
 
 PROG=sudo
-VER=1.9.4p1
+VER=1.9.4p2
 PKG=security/sudo
 SUMMARY="Authority delegation tool"
 DESC="Provide limited super-user privileges to specific users"
