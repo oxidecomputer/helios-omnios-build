a73fa32e3a0c681925b7c2e072bca22b04b3bbc3
Dominik Hassler <hadfl@omnios.org>
2021-01-05 16:39:17 +0000

    libpcap: update to 1.10.0

--- build/libpcap/build.sh
+++ build/libpcap/build.sh
@@ -13,12 +13,12 @@
 # }}}
 #
 # Copyright 2017 OmniTI Computer Consulting, Inc.  All rights reserved.
-# Copyright 2019 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2021 OmniOS Community Edition (OmniOSce) Association.
 
 . ../../lib/functions.sh
 
 PROG=libpcap
-VER=1.9.1
+VER=1.10.0
 PKG=system/library/pcap
 SUMMARY="libpcap - a packet capture library"
 DESC="$SUMMARY"
@@ -29,17 +29,12 @@ CONFIGURE_OPTS="
 "
 LDFLAGS+=" $SSPFLAGS"
 
-fixup_man3(){
-    mv $DESTDIR/usr/share/man/man3 $DESTDIR/usr/share/man/man3pcap
-}
-
 init
 download_source $PROG $PROG $VER
 patch_source
 prep_build
 build
 make_isa_stub
-fixup_man3
 make_package
 clean_up
 
