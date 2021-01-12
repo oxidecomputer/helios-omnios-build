f9521aa27e1b2218e0de3762affa26f41214ada0
Andy Fiddaman <omnios@citrus-it.co.uk>
2021-01-12 11:46:29 +0000

    sudo: update to 1.9.5p1

--- build/sudo/build.sh
+++ build/sudo/build.sh
@@ -13,12 +13,12 @@
 # }}}
 #
 # Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
-# Copyright 2020 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2021 OmniOS Community Edition (OmniOSce) Association.
 
 . ../../lib/functions.sh
 
 PROG=sudo
-VER=1.9.4p2
+VER=1.9.5p1
 PKG=security/sudo
 SUMMARY="Authority delegation tool"
 DESC="Provide limited super-user privileges to specific users"
