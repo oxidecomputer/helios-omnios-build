02d749b5f60f73833579d5751c12b4815811beae
Andy Fiddaman <omnios@citrus-it.co.uk>
2021-03-03 17:13:46 +0000

    openssh: update to 8.5p1

--- build/openssh/build.sh
+++ build/openssh/build.sh
@@ -13,12 +13,12 @@
 # }}}
 #
 # Copyright 2015 OmniTI Computer Consulting, Inc.  All rights reserved.
-# Copyright 2020 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2021 OmniOS Community Edition (OmniOSce) Association.
 
 . ../../lib/functions.sh
 
 PROG=openssh
-VER=8.4p1
+VER=8.5p1
 PKG=network/openssh
 SUMMARY="OpenSSH Client and utilities"
 DESC="OpenSSH Secure Shell protocol Client and associated Utilities"
