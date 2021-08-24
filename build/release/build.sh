0fe6c01af54472abf3953b21b4f55820df10fd0d
Dominik Hassler <hadfl@omnios.org>
2021-08-24 17:47:54 +0000

    preparing for r151038q

--- build/release/build.sh
+++ build/release/build.sh
@@ -23,7 +23,7 @@ SUMMARY="OmniOS release information"
 DESC="OmniOS /etc/release and /etc/os-release files"
 
 # Update the following line with the current release suffix
-RELSUFFIX=o
+RELSUFFIX=q
 
 RELEASE=${RELVER}${RELSUFFIX}
 RELDATE="`date +%Y.%m.%d`"
