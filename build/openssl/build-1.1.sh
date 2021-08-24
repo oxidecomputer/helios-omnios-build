8fa4dab60354fa77665197dd088ed84c5c22a4aa
Dominik Hassler <hadfl@omnios.org>
2021-08-24 17:14:23 +0000

    openssl-11: update to 1.1.1l

--- build/openssl/build-1.1.sh
+++ build/openssl/build-1.1.sh
@@ -18,7 +18,7 @@
 . ../../lib/functions.sh
 
 PROG=openssl
-VER=1.1.1k
+VER=1.1.1l
 PKG=library/security/openssl-11
 SUMMARY="Cryptography and SSL/TLS Toolkit"
 DESC="A toolkit for Secure Sockets Layer and Transport Layer protocols "
