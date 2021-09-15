8cb4d50f0663fb869d48ba7f0e894ac5fc4b5fc5
Andy Fiddaman <omnios@citrus-it.co.uk>
2021-09-15 11:07:06 +0000

    curl - update from 7.78.0 to 7.79.0

--- build/curl/build.sh
+++ build/curl/build.sh
@@ -18,7 +18,7 @@
 . ../../lib/functions.sh
 
 PROG=curl
-VER=7.78.0
+VER=7.79.0
 PKG=web/curl
 SUMMARY="Command line tool for transferring data with URL syntax"
 DESC="Curl is a command line tool for transferring data with URL syntax, "
