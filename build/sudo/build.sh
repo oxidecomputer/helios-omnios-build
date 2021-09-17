ae9a79c7ca7d008f6e8da248bd41627ee1fbbb61
Andy Fiddaman <omnios@citrus-it.co.uk>
2021-09-17 09:21:15 +0000

    sudo - update from 1.9.8 to 1.9.8p1

--- build/sudo/build.sh
+++ build/sudo/build.sh
@@ -18,7 +18,7 @@
 . ../../lib/functions.sh
 
 PROG=sudo
-VER=1.9.8
+VER=1.9.8p1
 PKG=security/sudo
 SUMMARY="Authority delegation tool"
 DESC="Provide limited super-user privileges to specific users"
