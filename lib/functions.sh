f19b242f82498720c92783b333874083390e3fd0
Andy Fiddaman <omnios@citrus-it.co.uk>
2021-11-18 19:02:08 +0000

    tzcnt is ok even on CPUs without BMI

--- lib/functions.sh
+++ lib/functions.sh
@@ -2864,7 +2864,7 @@ check_bmi() {
     while read obj; do
         [ -f "$DESTDIR/$obj" ] || continue
         dis $DESTDIR/$obj 2>/dev/null \
-            | $RIPGREP -wq --no-messages 'mulx|[lt]zcnt|shlx' \
+            | $RIPGREP -wq --no-messages 'mulx|lzcnt|shlx' \
             && echo "$obj has been built with BMI instructions" \
             >> $TMPDIR/rtime.bmi &
         parallelise $LCPUS
