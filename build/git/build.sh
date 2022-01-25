6d6ec36f5ea5b5b0f5ea14c23bc3dcc7744badec
Andy Fiddaman <omnios@citrus-it.co.uk>
2022-01-25 10:54:39 +0000

    git - update from 2.34.1 to 2.35.0

--- build/git/build.sh
+++ build/git/build.sh
@@ -14,12 +14,12 @@
 #
 # Copyright (c) 2014 by Delphix. All rights reserved.
 # Copyright 2017 OmniTI Computer Consulting, Inc.  All rights reserved.
-# Copyright 2021 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2022 OmniOS Community Edition (OmniOSce) Association.
 
 . ../../lib/build.sh
 
 PROG=git
-VER=2.34.1
+VER=2.35.0
 PKG=developer/versioning/git
 SUMMARY="$PROG - distributed version control system"
 DESC="Git is a free and open source distributed version control system "
