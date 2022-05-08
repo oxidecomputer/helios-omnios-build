712ff306b6281b4cdbec1e06b1ce5b41763586b2
Andy Fiddaman <omnios@citrus-it.co.uk>
2022-05-08 09:06:48 +0000

    git - update from 2.36.0 to 2.36.1

--- build/git/build.sh
+++ build/git/build.sh
@@ -19,7 +19,7 @@
 . ../../lib/build.sh
 
 PROG=git
-VER=2.36.0
+VER=2.36.1
 PKG=developer/versioning/git
 SUMMARY="$PROG - distributed version control system"
 DESC="Git is a free and open source distributed version control system "
