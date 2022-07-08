37124ddbcbc3b5df87bfd2bd6c9068b2cbdde3dc
Andy Fiddaman <omnios@citrus-it.co.uk>
2022-07-08 22:41:59 +0000

    setuptools - update from 62.6.0 to 63.1.0

--- build/python310/setuptools/build.sh
+++ build/python310/setuptools/build.sh
@@ -18,7 +18,7 @@
 
 PKG=library/python-3/setuptools-310
 PROG=setuptools
-VER=62.6.0
+VER=63.1.0
 SUMMARY="Python package management"
 DESC="Easily download, build, install, upgrade, and uninstall Python packages"
 
