00542ec5691dcf052aef325195d3d0b1f4d86a23
Andy Fiddaman <illumos@fiddaman.net>
2022-09-05 23:00:32 +0000

    gawk - update from 5.1.1 to 5.2.0

--- build/gawk/build.sh
+++ build/gawk/build.sh
@@ -13,12 +13,12 @@
 # }}}
 #
 # Copyright 2011-2015 OmniTI Computer Consulting, Inc.  All rights reserved.
-# Copyright 2021 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2022 OmniOS Community Edition (OmniOSce) Association.
 #
 . ../../lib/build.sh
 
 PROG=gawk
-VER=5.1.1
+VER=5.2.0
 PKG=text/gawk
 SUMMARY="GNU awk"
 DESC="gawk - GNU implementation of awk"
