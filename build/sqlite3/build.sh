82cff3e8054a93e29dc422ae023964ac5a7358ca
Andy Fiddaman <illumos@fiddaman.net>
2022-11-23 15:23:43 +0000

    sqlite3 - update from 3390400 to 3400000

--- build/sqlite3/build.sh
+++ build/sqlite3/build.sh
@@ -18,7 +18,7 @@
 . ../../lib/build.sh
 
 PROG=sqlite-autoconf
-VER=3390400
+VER=3400000
 PKG=database/sqlite-3
 SUMMARY="SQL database engine library"
 DESC="SQLite is a self-contained, high-reliability, embedded, full-featured, "
