1d957c77491930281d7ce6aefa251863312dc933
Andy Fiddaman <illumos@fiddaman.net>
2023-01-04 18:31:28 +0000

    cryptography - update from 38.0.3 to 39.0.0

--- build/python310/cryptography/build.sh
+++ build/python310/cryptography/build.sh
@@ -12,13 +12,13 @@
 # http://www.illumos.org/license/CDDL.
 # }}}
 #
-# Copyright 2022 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2023 OmniOS Community Edition (OmniOSce) Association.
 #
 . ../../../lib/build.sh
 
 PKG=library/python-3/cryptography-310
 PROG=cryptography
-VER=38.0.3
+VER=39.0.0
 SUMMARY="Cryptographic recipes and primitives"
 DESC="$SUMMARY"
 
