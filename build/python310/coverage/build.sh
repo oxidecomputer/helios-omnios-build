e31ef57496f30a561646ade2e7358d04be0f97d0
Andy Fiddaman <illumos@fiddaman.net>
2023-01-04 18:31:27 +0000

    coverage - update from 6.5.0 to 7.0.3

--- build/python310/coverage/build.sh
+++ build/python310/coverage/build.sh
@@ -12,13 +12,13 @@
 # http://www.illumos.org/license/CDDL.
 # }}}
 #
-# Copyright 2022 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2023 OmniOS Community Edition (OmniOSce) Association.
 
 . ../../../lib/build.sh
 
 PKG=library/python-3/coverage-310
 PROG=coverage
-VER=6.5.0
+VER=7.0.3
 SUMMARY="Python coverage module"
 DESC="Code coverage testing for Python"
 
