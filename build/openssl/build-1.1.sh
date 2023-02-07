2fab51c2ef10de0401e37b5448fd14c787079634
Andy Fiddaman <illumos@fiddaman.net>
2023-02-07 16:10:14 +0000

    openssl: update to 3.0.8/1.1.1t

--- build/openssl/build-1.1.sh
+++ build/openssl/build-1.1.sh
@@ -13,13 +13,13 @@
 # }}}
 #
 # Copyright 2017 OmniTI Computer Consulting, Inc.  All rights reserved.
-# Copyright 2022 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2023 OmniOS Community Edition (OmniOSce) Association.
 #
 . ../../lib/build.sh
 . common.sh
 
 PROG=openssl
-VER=1.1.1s
+VER=1.1.1t
 PKG=library/security/openssl-11
 SUMMARY="Cryptography and SSL/TLS Toolkit"
 DESC="A toolkit for Secure Sockets Layer and Transport Layer protocols "
