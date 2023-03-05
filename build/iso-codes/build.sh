6edf46562333f3223abae877617079200dcc60ae
Andy Fiddaman <illumos@fiddaman.net>
2023-03-05 11:15:59 +0000

    iso-codes - update from 4.12.0 to 4.13.0

--- build/iso-codes/build.sh
+++ build/iso-codes/build.sh
@@ -13,12 +13,12 @@
 # }}}
 
 # Copyright 2017 OmniTI Computer Consulting, Inc.  All rights reserved.
-# Copyright 2022 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2023 OmniOS Community Edition (OmniOSce) Association.
 
 . ../../lib/build.sh
 
 PROG=iso-codes
-VER=4.12.0
+VER=4.13.0
 PKG=data/iso-codes
 SUMMARY="ISO code lists and translations"
 DESC="Lists of various ISO standards "
