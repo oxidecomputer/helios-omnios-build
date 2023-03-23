ceff97ef61d8724b3a9d13e9ec4f0a0fa1ff7979
Andy Fiddaman <illumos@fiddaman.net>
2023-03-23 19:40:38 +0000

    xz - update from 5.4.1 to 5.4.2

--- build/xz/build.sh
+++ build/xz/build.sh
@@ -17,7 +17,7 @@
 . ../../lib/build.sh
 
 PROG=xz
-VER=5.4.1
+VER=5.4.2
 PKG=compress/xz
 SUMMARY="XZ Utils - general-purpose data compression software"
 DESC="Free general-purpose data compression software with a "
