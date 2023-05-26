7bbfaff07c22cb06962103b1b2ad57002594770d
Andy Fiddaman <illumos@fiddaman.net>
2023-05-26 17:12:59 +0000

    curl - update from 8.1.0 to 8.1.1

--- build/curl/build.sh
+++ build/curl/build.sh
@@ -18,7 +18,7 @@
 . ../../lib/build.sh
 
 PROG=curl
-VER=8.1.0
+VER=8.1.1
 PKG=web/curl
 SUMMARY="Command line tool for transferring data with URL syntax"
 DESC="Curl is a command line tool for transferring data with URL syntax, "
