42e3875bf66b8ffc4b2bf8460d969eb922835b3d
Andy Fiddaman <illumos@fiddaman.net>
2023-06-06 13:29:59 +0000

    openssl-11: update from 1.1.1o to 1.1.1u

--- build/openssl/build-1.1.sh
+++ build/openssl/build-1.1.sh
@@ -18,7 +18,7 @@
 . ../../lib/functions.sh
 
 PROG=openssl
-VER=1.1.1o
+VER=1.1.1u
 PKG=library/security/openssl-11
 SUMMARY="Cryptography and SSL/TLS Toolkit"
 DESC="A toolkit for Secure Sockets Layer and Transport Layer protocols "
