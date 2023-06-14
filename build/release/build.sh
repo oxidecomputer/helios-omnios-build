727200ca45b2f7e2313ddf804623bc5b2e720f31
Andy Fiddaman <illumos@fiddaman.net>
2023-06-14 22:59:32 +0000

    Preparing for r151046h

--- build/release/build.sh
+++ build/release/build.sh
@@ -25,7 +25,7 @@ DESC="OmniOS /etc/release and /etc/os-release files"
 set_arch 64
 
 # Update the following line with the current release suffix
-RELSUFFIX=e
+RELSUFFIX=h
 
 RELEASE=${RELVER}${RELSUFFIX}
 RELDATE="`date +%Y.%m.%d`"
