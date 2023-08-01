75f8f91592502eb1eef2800a585512fb4aee78a0
Andy Fiddaman <illumos@fiddaman.net>
2023-08-01 17:49:23 +0000

    OpenSSL: update from 3.0.9 to 3.0.10

--- build/openssl/build-3.sh
+++ build/openssl/build-3.sh
@@ -19,7 +19,7 @@
 . common.sh
 
 PROG=openssl
-VER=3.0.9
+VER=3.0.10
 PKG=library/security/openssl-3
 SUMMARY="Cryptography and SSL/TLS Toolkit"
 DESC="A toolkit for Secure Sockets Layer and Transport Layer protocols "
