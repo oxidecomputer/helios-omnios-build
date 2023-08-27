54991c91419b2cb83c710b8133acba40aae4db5a
Andy Fiddaman <illumos@fiddaman.net>
2023-08-27 20:36:01 +0000

    python311 - update from 3.11.4 to 3.11.9

--- build/python311/build.sh
+++ build/python311/build.sh
@@ -12,12 +12,12 @@
 # http://www.illumos.org/license/CDDL.
 # }}}
 #
-# Copyright 2023 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2024 OmniOS Community Edition (OmniOSce) Association.
 #
 . ../../lib/build.sh
 
 PROG=Python
-VER=3.11.4
+VER=3.11.9
 PKG=runtime/python-311
 MVER=${VER%.*}
 SUMMARY="$PROG $MVER"
