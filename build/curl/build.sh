9fb21e02cafdbdcb904780f325c9186fd8819a6f
Andy Fiddaman <andy@oxidecomputer.com>
2023-10-12 12:14:44 +0100

    curl - update from 8.3.0 to 8.4.0 (#14)

--- build/curl/build.sh
+++ build/curl/build.sh
@@ -18,7 +18,7 @@
 . ../../lib/build.sh
 
 PROG=curl
-VER=8.3.0
+VER=8.4.0
 PKG=web/curl
 SUMMARY="Command line tool for transferring data with URL syntax"
 DESC="Curl is a command line tool for transferring data with URL syntax, "
