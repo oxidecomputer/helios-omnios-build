51e70b8fbacddf9265ddede9f9bae1d5e132e3f2
Andy Fiddaman <illumos@fiddaman.net>
2024-05-25 16:01:19 +0000

    chrony - update from 4.5 to 4.6.1

--- build/chrony/build.sh
+++ build/chrony/build.sh
@@ -17,7 +17,7 @@
 . ../../lib/build.sh
 
 PROG=chrony
-VER=4.5
+VER=4.6.1
 PKG=service/network/chrony
 SUMMARY="Network time services"
 DESC="A versatile implementation of the Network Time Protocol (NTP)"
