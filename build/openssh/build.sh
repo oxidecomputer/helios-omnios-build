c3787b9701cbb92551999e78677573e0fca92753
Andy Fiddaman <illumos@fiddaman.net>
2024-07-01 16:13:43 +0000

    openssh: Disable async-signal-unsafe code

--- build/openssh/build.sh
+++ build/openssh/build.sh
@@ -13,12 +13,13 @@
 # }}}
 #
 # Copyright 2015 OmniTI Computer Consulting, Inc.  All rights reserved.
-# Copyright 2023 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2024 OmniOS Community Edition (OmniOSce) Association.
 
 . ../../lib/build.sh
 
 PROG=openssh
 VER=9.6p1
+DASHREV=1
 PKG=network/openssh
 SUMMARY="OpenSSH Client and utilities"
 DESC="OpenSSH Secure Shell protocol Client and associated Utilities"
