8d032c2f2104238282c54baad4e2e43b15c1f683
Andy Fiddaman <illumos@fiddaman.net>
2026-04-10 21:36:34 +0000

    openssl -update from 3.5.5 to 3.5.6

--- build/openssl/build-3.sh
+++ build/openssl/build-3.sh
@@ -19,7 +19,7 @@
 . common.sh
 
 PROG=openssl
-VER=3.5.5
+VER=3.5.6
 PKG=library/security/openssl-3
 SUMMARY="Cryptography and SSL/TLS Toolkit"
 DESC="A toolkit for Secure Sockets Layer and Transport Layer protocols "
