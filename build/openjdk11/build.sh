5ae4e9ed63acc3f3fef7265472ddfc9a3abd2dab
Andy Fiddaman <illumos@fiddaman.net>
2026-08-22 00:14:12 +0100

    openjdk11: update to 11.0.32.1+1

--- build/openjdk11/build.sh
+++ build/openjdk11/build.sh
@@ -17,7 +17,7 @@
 . ../../lib/build.sh
 
 PROG=openjdk
-VER=11.0.29+7
+VER=11.0.32.1+1
 PKG=runtime/java/openjdk11
 SUMMARY="openjdk ${VER%%.*}"
 DESC="Open-source implementation of the eleventh edition of the "
