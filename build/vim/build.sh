9979407993aa6049aa40583b14368dd5d19c72c0
Andy Fiddaman <illumos@fiddaman.net>
2026-09-15 10:43:04 +0000

    vim - update from 9.1.1202 to 9.2.1014

--- build/vim/build.sh
+++ build/vim/build.sh
@@ -13,13 +13,13 @@
 # }}}
 #
 # Copyright 2016 OmniTI Computer Consulting, Inc.  All rights reserved.
-# Copyright 2025 OmniOS Community Edition (OmniOSce) Association.
+# Copyright 2026 OmniOS Community Edition (OmniOSce) Association.
 #
 . ../../lib/build.sh
 
 PROG=vim
-VER=9.1
-PATCHLEVEL=1202
+VER=9.2
+PATCHLEVEL=1014
 PKG=editor/vim
 SUMMARY="Vi IMproved"
 DESC="Advanced text editor that provides the power of the UNIX vi editor "
