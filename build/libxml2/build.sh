141034e0a27fb538e868c9f0285dac636922d106
Andy Fiddaman <illumos@fiddaman.net>
2026-09-16 17:55:32 +0000

    libxml2 - update from 2.13.9 to 2.15.4

--- build/libxml2/build.sh
+++ build/libxml2/build.sh
@@ -18,28 +18,53 @@
 . ../../lib/build.sh
 
 PROG=libxml2
-VER=2.13.9
+VER=2.15.4
 PKG=library/libxml2
 SUMMARY="XML C parser and toolkit"
 DESC="Portable XML parser and toolkit library"
+
+# 2.14 - Binary compatibility is restricted to versions 2.14 or newer. On ELF
+#        systems, the soname was bumped from libxml2.so.2 to libxml2.so.16.
+#        We continue to ship the last version from before this switch.
+PVERS="2.13.9"
 
 RUN_DEPENDS_IPS="compress/xz library/zlib"
 
 CONFIGURE_OPTS+="
     --disable-static
     --without-python
-    --with-legacy
 "
 
 TESTSUITE_FILTER="^(Total|[Tt]esting|Ran)"
 
+CPPFLAGS+=" -D_REENTRANT"
 CFLAGS[aarch64]+=" -mtls-dialect=trad"
 
 init
+prep_build
+
+# Skip previous versions for cross compilation
+# XXXARM: since illumos depends on libxml2, we ship the previous version
+# for now to ease transition
+#pre_build() { ! cross_arch $1; }
+
+# Build previous versions
+save_variables BUILDDIR EXTRACTED_SRC CONFIGURE_OPTS
+CONFIGURE_OPTS+=" --with-legacy"
+for pver in $PVERS; do
+    note -n "Building previous version: $pver"
+    set_builddir $PROG-$pver
+    download_source -dependency $PROG $PROG $pver
+    patch_source patches-${pver%%.*}
+    ((EXTRACT_MODE == 0)) && build
+done
+restore_variables BUILDDIR EXTRACTED_SRC CONFIGURE_OPTS
+unset -f pre_build
+
+note -n "Building current version: $VER"
 download_source $PROG $PROG $VER
 patch_source
 run_autoreconf -fi
-prep_build
 build
 run_testsuite check
 make_isa_stub
